15c5a0e2da10f20cf283850673fe3cb0297ded5f
DarenGit28 <daren.shamoun28@gmail.com>
2022-08-30 16:55:33 +0900

    commentsTab

--- src/pages/Post/tabs/CommentsTab.tsx
+++ src/pages/Post/tabs/CommentsTab.tsx
@@ -1,12 +1,80 @@
-import { Box, Stack } from "@mui/material";
+import { Avatar, Button, Divider, Stack, TextField, Typography, useTheme } from "@mui/material";
+import * as yup from 'yup';
+import { useFormik } from "formik";
 import Testimonial from "../../../components/Testimonial";
+import useAuth from "../../../hooks/useAuth";
+import { fetchFirstLettersFromName } from "../../../utils/functions";
+import { Icon } from "@iconify/react";
+
+const validSchema = yup.object().shape({
+  comment: yup.string().required('Please input some words.')
+});
 
 export default function CommentsTab() {
+  const theme = useTheme()
+  const { currentUser } = useAuth()
+
+  const formik = useFormik({
+    initialValues: {
+      comment: ''
+    },
+    validationSchema: validSchema,
+    onSubmit: (values) => {
+    }
+  })
+
   return (
-    <Box>
-      <Stack spacing={3}>
+    <Stack spacing={3}>
+      <Stack spacing={2}>
         <Testimonial />
       </Stack>
-    </Box>
+
+      <Divider />
+
+      <Stack direction="row" spacing={3}>
+        {
+          currentUser?.avatar ? (
+            <Avatar
+              component="img"
+              src={currentUser?.avatar}
+              alt=""
+            />
+          ) : (
+            <Avatar sx={{ bgcolor: theme.palette.primary.main }}>
+              {fetchFirstLettersFromName(`${currentUser?.first_name} ${currentUser?.last_name}`)}
+            </Avatar>
+          )
+        }
+
+        <Stack spacing={1} flexGrow={1}>
+          <TextField
+            name="comment"
+            placeholder="Comment here."
+            fullWidth
+            multiline
+            rows={5}
+            value={formik.values.comment}
+            onChange={formik.handleChange}
+            error={formik.touched.comment && Boolean(formik.errors.comment)}
+            helperText={
+              formik.touched.comment && formik.errors.comment ? (
+                <Typography
+                  component="span"
+                  sx={{ display: 'flex', alignItems: 'center', mx: 0 }}
+                >
+                  <Icon icon="bxs:error-alt" />&nbsp;
+                  {formik.touched.comment && formik.errors.comment}
+                </Typography>
+              ) : (<></>)
+            }
+          />
+          <Stack direction="row" justifyContent="end">
+            <Button variant="contained" onClick={() => formik.handleSubmit()}>
+              Submit
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   )
 }
