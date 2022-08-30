5d0dc8880e503406e313c41c53d47ac9599a769b
DarenGit28 <daren.shamoun28@gmail.com>
2022-08-30 16:41:30 +0900

    Testimonial update

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Box, Stack, Typography, useTheme } from "@mui/material";
+import { Icon } from "@iconify/react";
+import { Avatar, Box, Stack, Typography, useTheme, Icon as MuiIcon } from "@mui/material";
 
 export default function Testimonial() {
   const theme = useTheme()
@@ -9,8 +10,8 @@ export default function Testimonial() {
       >N</Avatar>
 
       <Box position="relative" bgcolor="#cccccc" px={3} py={2} borderRadius={3} width="100%">
-        <Box 
-          position="absolute" 
+        <Box
+          position="absolute"
           width={0}
           height={0}
           borderTop="20px solid #cccccc"
@@ -21,6 +22,14 @@ export default function Testimonial() {
         <Typography variant="body1" textAlign="justify" sx={{ wordBreak: 'break-all' }}>
           weggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggregegewrgaweeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeergggggggggggggggggggggggggggggggggggggggggggggggggggg
         </Typography>
+        <Stack direction="row" justifyContent="end" alignItems="center" mt={1}>
+          <MuiIcon>
+            <Icon icon="healthicons:i-schedule-school-date-time" />
+          </MuiIcon>
+          <Typography variant="body2">
+            20:36 May 9, 2019
+          </Typography>
+        </Stack>
       </Box>
     </Stack>
   )
